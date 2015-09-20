4f6d518350717e6ae46bbdd30cd6b0fd6f2f16ef
Josh Gillies <github@joshgilli.es>
2015-09-20 22:46:08 +1000

    Add support for setting asset attributes, and creating children

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var extend = require('xtend')
+
 module.exports = function asset (type, opts, children) {
   var asset = {}
 
@@ -6,6 +8,17 @@ module.exports = function asset (type, opts, children) {
   }
 
   asset.type = type
+
+  if (children && children.length && Array.isArray(children)) {
+    asset.children = children.map(function getChild (child) {
+      if (typeof child === 'function') {
+        return child()
+      }
+      return child
+    })
+  }
+
+  asset = extend(opts, asset)
 
   return asset
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var asset = require('./')
 var test = require('tape')
 
-test('exports a function', function (assert) {
+test('simple', function (assert) {
   assert.plan(1)
   assert.deepEqual(asset('folder'), { type: 'folder' })
 })
@@ -13,4 +13,33 @@ test('error if type is undefined', function (assert) {
   } catch (error) {
     assert.ok(error)
   }
+})
+
+test('set asset properties', function (assert) {
+  assert.plan(2)
+
+  var expected = { type: 'folder', name: 'Sites', link: 'type_2' }
+  var tests = {
+    'pass opts object': { name: 'Sites', link: 'type_2' },
+    'cannot override asset type with opts.type': { type: 'nope', name: 'Sites', link: 'type_2' }
+  }
+
+  for (test in tests) {
+    assert.deepEqual(asset('folder', tests[test]), expected, test)
+  }
+})
+
+test('create children assets', function (assert) {
+  assert.plan(1)
+
+  var assets = {
+    'folder': { name: 'Sites', link: 'type_2' },
+    'site': { name: 'My Site' }
+  }
+  var expected = { type: 'folder', name: 'Sites', link: 'type_2', children: [{ type: 'site', name: 'My Site' }] }
+  var test = asset('folder', assets['folder'], [
+    asset('site', assets['site'])
+  ])
+
+  assert.deepEqual(test, expected, 'processed children')
 })
